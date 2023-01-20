a89d6dbdc0484322befdbcc31559214ef1145f3a
wonhj <wonhjteacher@gmail.com>
2023-01-20 13:26:53 +0900

    weather

--- weather.js
+++ weather.js
@@ -2,6 +2,7 @@ const API_KEY=`1f8ed734823781216a3c57170a63d19b`
 const country = document.querySelector('#country')
 const area = document.querySelector('#area')
 const currentTemp = document.querySelector('#currentTemp')
+const currentDescription = document.querySelector('#currentDescription')
 const maxTemp  = document.querySelector('#maxTemp')
 const minTemp   = document.querySelector('#minTemp')
 const citybtn = document.querySelectorAll('.citybtn');
@@ -14,33 +15,27 @@ const getCurrentLocation = () => {
         getCurrentWeater(lat,lon)
     })
 }
-
 const getCurrentWeater = async (lat,lon) => {
-    let url = new URL(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
+    let url = new URL(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`);
     let response = await fetch(url);
     let data = await response.json();
     console.log('data',data )
-    country.innerText = data.sys.country;
-    area.innerText = data.name;
-    currentTemp.innerText = data.main.temp
-    maxTemp.innerText = data.main.temp_max;
-    minTemp.innerText = data.main.temp_min;
+    render(data)
 }
-
 const getCityWeater = async (e) => {
     let city = e.target.dataset.cityname;
-    let url = new URL(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&`);
+    let url = new URL(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&lang=kr`);
     let response = await fetch(url);
     let data = await response.json();
-    console.log('data',data )
+    render(data)
+}
+const render = (data) => {
     country.innerText = data.sys.country;
+    currentDescription.innerText = data.weather[0].description;
     area.innerText = data.name;
     currentTemp.innerText = data.main.temp
     maxTemp.innerText = data.main.temp_max;
     minTemp.innerText = data.main.temp_min;
 }
-
-
-
 
 getCurrentLocation();
